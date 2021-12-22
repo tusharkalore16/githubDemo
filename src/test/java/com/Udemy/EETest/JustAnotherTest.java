f8287e7b4792782f8651dc3a02d0675357fabb78
Tushar Kalore <tusharkalore16@gmail.com>
2021-12-22 17:54:37 +0530

    commit to develop branch by me

---
+++ src/test/java/com/Udemy/EETest/JustAnotherTest.java
@@ -0,0 +1,20 @@
+package com.Udemy.EETest;
+
+import org.testng.annotations.Test;
+
+public class JustAnotherTest {
+	@Test
+	public void gitDemo4() {
+		System.out.println("New demoTest added ");
+	}
+	
+	@Test
+	public void gitDemo5() {
+		System.out.println("New demoTest added ");
+	}
+	
+	@Test
+	public void gitDemo6() {
+		System.out.println("Another Test Added"); 
+	}
+}
